3560c24f454b34d518d7a80f86eeca0e6cc5d5f3
Bien Justin Mandac <justinmandac@gmail.com>
2014-07-29 22:35:14 +0800

    accepts command line arguments

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-var process = require('child_process');
-var cmd_str = 'node client.js http://localhost:9001/';
+var spawn = require('child_process');
+var CHILD_COUNT = process.argv[2];
+var SOCKET_ADDR = process.argv[3];
+var cmd_str = 'node client.js '+SOCKET_ADDR;
 
-for(var i = 0 ; i < 5; i++){
-	var client = process.exec(cmd_str, function(error,stdout,stderr){
+for(var i = 0 ; i < CHILD_COUNT; i++){
+	var client = spawn.exec(cmd_str, function(error,stdout,stderr){
 		if(error){
 			console.log(error.stack);
 		}
