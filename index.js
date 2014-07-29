2b19a053581c688f230f45ea5a4b117e72dc3521
Bien Justin Mandac <justinmandac@gmail.com>
2014-07-29 23:40:30 +0800

    used fork instead of spawning new processes

--- index.js
+++ index.js
@@ -5,8 +5,10 @@ var CHILD_COUNT  = process.argv[2];
 var SOCKET_ADDR  = process.argv[3];
 var CLIENT_OPTION= process.argv[4];
 var SERVER_OPTION= process.argv[5];
-var client_str = 'node client.js '+SOCKET_ADDR+' '+CLIENT_OPTION;
-var server_str = 'node server.js '+CHILD_COUNT+' '+SERVER_OPTION;
+var client_scr = 'client.js';
+var server_scr = 'server.js';
+var client_str = 'node '+client_scr+' '+SOCKET_ADDR+' '+CLIENT_OPTION;
+var server_str = 'node '+server_scr+' '+CHILD_COUNT+' '+SERVER_OPTION;
 
 var server = spawn.exec(server_str,function(error,stdout,stderr){
 	if(error){
@@ -20,13 +22,7 @@ server.on('exit',function(code){
 });
 
 for(var i = 0 ; i < CHILD_COUNT; i++){
-	var client = spawn.exec(client_str, function(error,stdout,stderr){
-		if(error){
-			console.log(error.stack);
-		}
-		console.log('stdout: '+stdout);
-		console.log('stderr: '+stderr);
-	});
+	var client = spawn.fork(client_scr,[SOCKET_ADDR,CLIENT_OPTION]);
 	client.on('exit',function(code){
 		console.log('[CLIENT] Child process exited with code '+code);
 	});
